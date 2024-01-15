da8124ff6b34010beb60f9cd5cf8a74a354ff1ac
Anya Rybkina <anuta.ribkina@gmail.com>
2024-01-15 17:14:52 +0200

    Дополнение

---
+++ Program.cs
@@ -0,0 +1,24 @@
+string[] arrayOne = new string[] {"Hello", "2", "world", ":-)"};
+string[] arrayTwo = new string[arrayOne.Length];
+void SecondArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+} 
+SecondArray(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
