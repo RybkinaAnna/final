541e106efc8d75a47360b02fd2f4e1e36822d183
Anya Rybkina <anuta.ribkina@gmail.com>
2024-01-15 16:45:06 +0200

    Этап 2

--- ikr/Program.cs
+++ ikr/Program.cs
@@ -1,2 +1,14 @@
 string[] arrayOne = new string[] {"Hello", "2", "world", ":-)"};
 string[] arrayTwo = new string[arrayOne.Length];
+void SecondArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
